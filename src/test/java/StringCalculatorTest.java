e3310fb5d20c0e8940a67581b2d6f003fe4431f3
David Caldwell <david.caldwell@bellevuecollege.edu>
2022-06-21 13:15:30 -0700

    Add files created during exam

--- src/test/java/StringCalculatorTest.java
+++ src/test/java/StringCalculatorTest.java
@@ -3,10 +3,33 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class StringCalculatorTest {
-
+	StringCalculator test;
+	
+	@BeforeEach
+	public void before() {
+		test = new StringCalculator();
+	}
+	
+	@AfterEach
+	public void after() {
+		test = null
+	}
+	
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void test1() {
+		assertThrows(IllegalArgumentException.class, () -> {test.add("-1","10")});
+	}
+	public void test1a() {
+		assertThrows(IllegalArgumentException.class, () -> {test.add("0","-1")});
+	}
+	@Test
+	public void test2() {
+		assertEquals("1",test.add("1","10000000"));
+	}
+	
+	@Test
+	public void test2a() {
+		assertEquals("1",test.add("100000000","1"));
 	}
 
 }
